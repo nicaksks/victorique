f5d8bd68084f011c0fcf0d23ab08747e7db7d0bf
nica <100009274+nicaksks@users.noreply.github.com>
2024-04-20 09:31:38 -0300

    Delete main.rs

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use victorique::logger::terminal::{Constructor, Logger};
-
-fn main() {
-    Logger.fatal("Hello, World!");
-    Logger.error("Hello, World!");
-    Logger.warn("Hello, World!");
-    Logger.debug("Hello, World!");
-    Logger.info("Hello, World!");
-}
